739225d2fd812b6d4472155f7e1bc8f4c8f2f974
Tatyta13 <joa.diazb@duocuc.cl>
2024-11-25 20:03:55 -0300

    experiencia3

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  //apiUrl:'http://localhost:3000'
+  //apiUrl: 'https://github.com/Tatyta13/RestData011V'
+  apiUrl:'https://github.com/ArielJCM/CheckinApp'
+};
